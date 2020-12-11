d62741b1088d04d06194c81f5cc0c02dc9d8f19a
sidnei-paixao <sidnei.paixao@etec.sp.gov.br>
2020-12-11 17:26:00 -0300

    Inserção do objeto IMG e criação do mapa clicável

--- regexImagemExterna.js
+++ regexImagemExterna.js
@@ -2,3 +2,8 @@
 const arrumador = (imagem) => {
   return imagem.replace(/^<img +(\w*"[a-zA-Z].*)*src="|".*/g, "");
 };
+
+const fnExtraiImagem = (cImagem) => {
+  return cImagem.match(/\<img.+src\=(?:\"|\')(.+?)(?:\"|\')(?:.+?)\>/)[1];
+};
+
